dbe334fb6ca3ae79f8865ffd6869f5d2921d49d5
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-09-09 15:26:54 +0200

    auth routers; error handling and response normalization

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,12 +1,26 @@
 import express from 'express';
+import 'express-async-errors'
 import {json} from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
+import { erroHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!');
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all('*', async () => {
+    throw new NotFoundError();
 });
+
+app.use(erroHandler);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
