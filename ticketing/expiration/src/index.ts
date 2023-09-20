afd6bb4bf28418d689f8f5baf60d1ba6e4408bf8
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-09-20 23:38:04 +0200

    test multiple deployments

--- ticketing/expiration/src/index.ts
+++ ticketing/expiration/src/index.ts
@@ -1,5 +1,5 @@
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
-import { natsWrapper } from './nats-wrapper';
+import { natsWrapper } from './nats-wrapper'; 
 
 const startNats = async () => {
     if (!process.env.NATS_CLUSTER_ID) {
