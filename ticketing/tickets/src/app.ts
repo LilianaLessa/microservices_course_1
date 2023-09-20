bbd2a8d87350a085a1c998567b802c879f55bf8e
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-09-20 12:33:48 +0200

    introduce ticket creation

---
+++ ticketing/tickets/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import 'express-async-errors'
+import {json} from 'body-parser';
+import cookieSession from 'cookie-session';
+import { NotFoundError, currentUser, erroHandler } from '@liliana-lessa-microservices-1/common';
+import { createTicketRouter } from './routes/new';
+
+const app = express();
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+}));
+app.use(currentUser);
+
+app.use(createTicketRouter);
+
+app.all('*', async () => {
+    throw new NotFoundError();
+});
+
+app.use(erroHandler);
+
+export { app };
